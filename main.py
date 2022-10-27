a4b782d6d5c0d29b53cbc33cbf5282ba4db96e36
Filippo Biondi <filippo22@ru.is>
2022-10-27 02:01:33 +0000

    reverted main

--- main.py
+++ main.py
@@ -855,7 +855,7 @@ if __name__ == "__main__":
         # configure learning rate
         bs, base_lr = config.data.params.batch_size, config.model.base_learning_rate
         if not cpu:
-            ngpu = 1
+            ngpu = len(lightning_config.trainer.gpus.strip(",").split(','))
         else:
             ngpu = 1
         if 'accumulate_grad_batches' in lightning_config.trainer:
@@ -934,3 +934,4 @@ if __name__ == "__main__":
             os.rename(logdir, dst)
         if trainer.global_rank == 0:
             rank_zero_print(trainer.profiler.summary())
+
